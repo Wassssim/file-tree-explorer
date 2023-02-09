90db2502434cf3db272e983699222c612f30aff0
Wassssim <aprsn124@gmail.com>
2023-02-09 18:57:02 +0100

    added move/find node algorithms

--- src/algorithms/treeAlgorithms.ts
+++ src/algorithms/treeAlgorithms.ts
@@ -1,11 +1,14 @@
 import { ITreeNode } from "../types/fileExplorerData";
 
-export const addToTree = (tree: ITreeNode, folderId: string, node: ITreeNode): ITreeNode | null => {
+export const addToTree = (tree: ITreeNode | null, folderId: string, node: ITreeNode): ITreeNode | null => {
+    if (!tree)
+        return {...node};
+    
     if ((tree.isFolder === true) && (tree.id === folderId))
         return { ...tree, children: [...(tree?.children || []), { ...node }] }
 
     if (!tree.children)
-        return { ...tree };
+        return tree;
 
     const newChildren = [];
     for (const childNode of (tree.children || [])) {
@@ -17,24 +20,27 @@ export const addToTree = (tree: ITreeNode, folderId: string, node: ITreeNode): I
     if (newChildren.length > 0)
         return { ...tree, children: [...newChildren] }
 
-    return { ...tree };
+    return tree;
 }
 
-export const removeFromTree = (tree: ITreeNode, nodeId: string): ITreeNode | null => {
+export const removeFromTree = (tree: ITreeNode | null, nodeId: string, recursive: boolean = false): ITreeNode | null => {
+    if (!tree)
+        return null;
+    
     if (tree.id === nodeId) {
-        tree.children?.map((child: ITreeNode) => removeFromTree(child, child.id));
-        console.log("Removing child", tree.name);
+        if (recursive) {
+            tree.children?.map((child: ITreeNode) => removeFromTree(child, child.id));
+            console.log("Removing child", tree.name);
+        }
         return null;
     }
 
-    const newChildren = [];
-    for (const childNode of (tree.children || [])) {
-        const resultingTree = removeFromTree(childNode, nodeId);
-        if (resultingTree)
-            newChildren.push(resultingTree);
-    }
+    const newChildren = (tree.children?.map(childNode => removeFromTree(childNode, nodeId)).filter(x => x !== null) || []) as ITreeNode[];
 
-    return { ...tree, children: newChildren }
+    if (newChildren.length > 0)
+        return { ...tree, children: newChildren }
+    
+    return tree;
 }
 
 export const renameNode = (tree: ITreeNode, nodeId: string, newName: string): ITreeNode => {
@@ -52,4 +58,30 @@ export const renameNode = (tree: ITreeNode, nodeId: string, newName: string): IT
         return { ...tree, children: newChildren }
 
     return { ...tree }
+}
+
+// DFS
+export const findNodeById = (tree: ITreeNode, nodeId: string): ITreeNode | null => {
+    if (tree.id === nodeId)
+        return tree;
+    
+    for (let childNode of (tree.children || [])) {
+        const node = findNodeById(childNode, nodeId);
+        if (node)
+            return node;
+    }
+
+    return null;
+}
+
+export const moveNode = (tree: ITreeNode, sourceNodeId: string, parentDestinationNode: ITreeNode): ITreeNode | null => {
+    if (!tree || !parentDestinationNode.isFolder || (sourceNodeId === parentDestinationNode.id))
+        return null;
+    
+    const sourceNode = findNodeById(tree, sourceNodeId);
+
+    if (!sourceNode)
+        return null;
+    
+    return addToTree(removeFromTree(tree, sourceNodeId, false), parentDestinationNode.id, sourceNode);
 }
